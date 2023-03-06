769a53d4c719339e5269425dc0506b8ab4422609
nicklo0531 <nicklo@uw.edu>
2023-03-05 19:55:41 -0800

    save 5

--- ps6/app.R
+++ ps6/app.R
@@ -23,7 +23,7 @@ ui <- fluidPage(
              tabPanel("Platform Plot",
                       sidebarLayout(
                         sidebarPanel(
-                          selectInput("videogame_platforms", "Select the platform", c("Wii", "PS", "PS2", "PS3", "PS4", "PSV",
+                          selectInput("videogame_platforms_plot", "Select the platform", c("Wii", "PS", "PS2", "PS3", "PS4", "PSV",
                                                                             "PC", "NES (Nintendo Entertainment System)",
                                                                             "GB (Game Boy)", "GBA (Game Boy Advance)", "DS (Dual Screen)",
                                                                             "x360 (Xbox360)", "SNES (Super Nintendo Entertainment System)",
@@ -32,20 +32,34 @@ ui <- fluidPage(
                                                                             "SAT", "SCD", "WS (Wonder Swan)", "NG", "TG16", "3DO", "GG",
                                                                             "PCFX"))),
                         mainPanel(
-                          plotOutput("videogame_platforms")
+                          plotOutput("videogame_platforms_plot")
                         )
                       )
              ),
              
-             tabPanel("Platform Table")
+             tabPanel("Platform Table",
+                      sidebarLayout(
+                        sidebarPanel(
+                          selectInput("videogame_platforms_table", "Select the platform", c("Wii", "PS", "PS2", "PS3", "PS4", "PSV",
+                                                                                           "PC", "NES (Nintendo Entertainment System)",
+                                                                                           "GB (Game Boy)", "GBA (Game Boy Advance)", "DS (Dual Screen)",
+                                                                                           "x360 (Xbox360)", "SNES (Super Nintendo Entertainment System)",
+                                                                                           "3DS", "N64 (Nintendo 64)", "XB (Xbox)", "2600 (Atari 2600)",
+                                                                                           "PSP", "XOne", "WiiU", "GC (GameCube)", "GEN", "DC",
+                                                                                           "SAT", "SCD", "WS (Wonder Swan)", "NG", "TG16", "3DO", "GG",
+                                                                                           "PCFX"))))),
+                       mainPanel(
+                         plotOutput("videogame_platforms_table")
+             )
+                      )
   )
-)
+
 
 
 
 ##Server
 server <- function(input, output) {
-  output$videogame_platforms <- renderPlot({
+  output$videogame_platforms_plot <- renderPlot({
     video_game %>% 
       select(Global_Sales,
              Year,
@@ -54,7 +68,7 @@ server <- function(input, output) {
              Platform %in% input$platforms) %>%
       group_by(Platform, Year) %>% 
       summarize(global_sales_in_that_year = mean(Global_Sales)) %>% 
-      ggplot(aes(x = Year, y = Global_Sales)) +
+      ggplot(aes(x = Year, y = global_sales_in_that_year)) +
       geom_line() +
       labs(x = "Year", y = "Sales in Million")
   })
